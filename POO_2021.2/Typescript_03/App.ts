ecf9426cb1fede93da0afbea88b0d58992a19640
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2021-10-12 20:18:18 -0300

    Add files via upload

---
+++ POO_2021.2/Typescript_03/App.ts
@@ -0,0 +1,33 @@
+/* Q 01
+Classes são modelos a partir dos quais os objetos são criados. Essas mesmas
+definem os comportamentos e características dos objetos.
+Uma classe é como um planejamento e pode gerar mais de um objeto.
+
+Já os objetos sao as classes em execução. E uma de suas pprincipais características é que
+possuem atributos e métodos.
+*/
+
+/* Q 02
+Os atributos estão relacionados as caracteristicas e propriedades que os objetos podem possuir.
+Além disso, ainda definem o estado daqueles mesmos objetos.
+Os métodos já estão relacionados aos comportamentos daquele objeto, ou seja, as ações que
+ele pode realizar.
+Exemplo:
+Objeto -- Conta bancária
+Atributos: Nome do titular, numero da conta e saldo.
+Métodos: Creditar, debitar, transferir, sacar.
+*/ 
+
+/* Q 03
+Atributos ---- Sistemas em que são importantes ---- Sistemas em que não são importantes
+
+Peso      ----    Sistema que calcula IMC      ---- Sistema de conta bancária
+Tipo de CNH ----  Sistema do DETRAN            ---- Cadastro de rede social
+Tipo Sanguíneo ---- Sistema de um hospital     ---- Sistema do DETRAN
+habilidade destra ---- criação de jogador no PES ---- Cadastro em rede social
+Percentual de gordura ---- Sistema de analise de saúde ---- Site de compra de imóveis
+Saldo em conta    ---- Sistema de conta bancária ---- Jogos para crianças
+Etnia     ---- Sistema de exames que possuem cotas racias/etnicas ---- Site de compras
+
+*/
+
